3a2f087e3bb71eec94d50aba6999a752c18a213a
kjdavis2423@gmail.com <kingdavis2423@gmail.com>
2016-11-02 12:57:17 -0400

    Cheese

---
+++ Coursera_Ruby/module2/ex_explicit.rb
@@ -0,0 +1,8 @@
+def two_times_explicit(&i_am_a_block)
+	return "No block" if i_am_a_block.nil?
+	i_am_a_block.call
+	i_am_a_block.call
+end
+
+puts two_times_explicit
+two_times_explicit {puts "Hello"}
