764415e529cc58d9415b1044cb4b252e79b803f5
kjdavis2423@gmail.com <kingdavis2423@gmail.com>
2016-11-02 14:10:28 -0400

    Cheese

---
+++ Coursera_Ruby/module2/ex_if.rb
@@ -0,0 +1,9 @@
+a = 5
+
+if a == 3
+	puts "a equals 3"
+elsif a == 5
+	puts "a is 5"
+else
+	puts "a is not 3 nor 5"
+end
---
+++ Coursera_Ruby/module2/ex_modifier.rb
@@ -0,0 +1,13 @@
+#if modifier form
+
+a =5
+b = 0
+
+puts "One liner" if a == 5 and b == 0
+
+
+# while modifier form
+
+times_2 = 2
+times_2 *= 2 while times _2 < 100
+puts times_2 
